28cc4bd7823ec1bcdfa24563ab895234efd39cfc
shubham <shubhamsagar628@gmail.com>
2025-02-17 20:50:46 +0530

    Second

---
+++ 01.js
@@ -0,0 +1,45 @@
+//3---Area Of Reactangle
+// const cala = (length,breadth) => {
+//     console.log(length*breadth)
+// }
+// const ans  = cala(2,2.4) 
+
+//4 ------Even or Odd
+// const cal = (a) => {
+//     if(a%2 === 0){
+//         return "This is even number"
+//     }else if(a%2 !== 0){
+//         return "This is odd number"
+//     }else{
+//         return "Please provide valid number"
+//     }
+// }
+
+// const ans  = cal(undefined)
+// console.log(typeof ans)
+
+//5----- smallest Number
+
+// function f(a,b,c)  {
+//    if(a<b && a<c){
+//     return a
+//    }else if(b<a && b<c){
+//     return b
+//    }else if(c<a && c<b){
+//     return c
+//    }
+//    return a
+// }
+
+// function f(a,b,c){
+//     let smallestNumeber = a
+//     if(b<smallestNumeber){
+//         smallestNumeber = b
+//     }
+//     if(c<smallestNumeber){
+//         smallestNumeber = c
+//     }
+//     return smallestNumeber
+// }
+
+// console.log(f(3,3,3))
